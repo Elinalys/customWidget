51b44956a4e8c6725b52279b08fc213e3d61c232
Elinalys <lili63830@gmail.com>
2021-09-03 14:06:38 +0200

    20

--- read_error/read_errors.js
+++ read_error/read_errors.js
@@ -6,18 +6,17 @@
 
     customElements.define('com-synvance-readerrors', class ReadErrorsSps extends HTMLElement {
 
-
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
+
+            this.$h1 = shadowRoot.querySelector('h1');
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
+            //this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -32,9 +31,7 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
-                this.redraw();
-            }
+            this.redraw();
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
@@ -52,6 +49,7 @@
         */
 
         redraw(){
+            this.$h1.innerHTML = "AOE"
         }
     });
 })();
