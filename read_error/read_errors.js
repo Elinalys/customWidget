60a939c0820f819c990f51932411643a315ca329
Elinalys <lili63830@gmail.com>
2021-09-03 14:25:51 +0200

    24

--- read_error/read_errors.js
+++ read_error/read_errors.js
@@ -1,18 +1,20 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello World</h1>
     `;
 
-    customElements.define('com-synvance-readerrors', class ReadErrorsSps extends HTMLElement {
+    customElements.define("com-synvance-readerrors-sps", class ReadErrors extends HTMLElement {
+
 
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
+            this._tagContainer;
+            this._tagType = "h1";
+            this._tagText = "Hello World";
 
-            //this.$h1 = shadowRoot.querySelector('h1');
-            
             //Adding event handler for click events
 			this.addEventListener("click", event => {
 				var event = new Event("onClick");
@@ -22,7 +24,8 @@
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this.redraw();
+            this._firstConnection = true;
+            this.redraw(); 
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -37,25 +40,59 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            this.redraw();
+            if (this._firstConnection){
+                this.redraw();
+            }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
+        
         }
 
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        // Commented out by default
         /*
         onCustomWidgetResize(width, height){
-            redraw()
+        
         }
         */
 
-        redraw(){
-            //this.$h1.innerHTML = "AOE"
+        //Getters and Setters
+        get widgetText() {
+            return this._tagType;
         }
+
+        set widgetText(value) {
+            this._tagText = value;
+        }
+
+
+        get headingType() {
+            return this._tagType;
+            }
+
+        set headingType(value) {
+            this._tagType = value;
+        }
+
+        // End - Getters and Setters
+
+        redraw(){
+            if (this._tagContainer){
+                this._tagContainer.parentNode.removeChild(this._tagContainer);
+            }
+
+            var shadow = window.getSelection(this._shadowRoot);
+            this._tagContainer = document.createElement(this._tagType);
+            var theText = document.createTextNode(this._tagText);    
+            this._tagContainer.appendChild(theText); 
+            this._shadowRoot.appendChild(this._tagContainer);
+
+        }
+    
+    
     });
+        
 })();
